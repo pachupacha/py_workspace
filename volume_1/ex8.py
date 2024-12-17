f02f3e5038103ad0b3d3147079063e7b7e1f16cb
pachupacha <pachupacha10@gmail.com>
2024-12-17 16:26:13 -0300

    feat: duplicate each character of the input string

---
+++ volume_1/ex8.py
@@ -0,0 +1,14 @@
+caracteres = input("Ingresa 5 caracteres: ")
+duplicated = (
+    caracteres[0] +
+    caracteres[0] +
+    caracteres[1] +
+    caracteres[1] +
+    caracteres[2] +
+    caracteres[2] +
+    caracteres[3] +
+    caracteres[3] +
+    caracteres[4] +
+    caracteres[4]
+)
+print(str(duplicated))
