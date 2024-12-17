0e4e4a7860ac9c5fd7bd6dee3f3299171b8f6e8e
pachupacha <pachupacha10@gmail.com>
2024-12-17 16:52:42 -0300

    feat: personalized greeting based on user name

---
+++ volume_1/ex11.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ").lower()
+
+if name == "javier":
+    name_capitalized = name.title()
+    print(f"Welcome {name_capitalized}, dear Mr. President of the Nation.")
+else:
+    name_capitalized = name.title()
+    print(f"Welcome {name_capitalized}, we hope your visit is to your liking.")
